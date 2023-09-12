ed0c139f16f04cc46d19e263d1ae47edf7534c64
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-09-12 12:05:24 +0700

    Initial commit from React + Vite
    This commit also configures Tailwind CSS and creates the project folder structure

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import Sidebar from '../../components/Sidebar';
+
+export default function HomePage() {
+  return (
+    <>
+      <Sidebar />
+      <div className="text-3xl font-bold underline">This is HomePage</div>
+    </>
+  );
+}
