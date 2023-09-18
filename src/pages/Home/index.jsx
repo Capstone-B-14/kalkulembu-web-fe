a2c066f1a5da03a232ba4b6dc9c83fc7acffdf7d
daffaromero <daffaromero@mail.ugm.ac.id>
2023-09-18 12:16:15 +0700

    Updated sidebar behavior

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,9 @@ import Sidebar from "../../components/Sidebar/sidebar";
 export default function HomePage() {
   return (
     <>
-      <Sidebar />
+      <div className='flex'>
+        <Sidebar />
+      </div>
       <div className='text-3xl font-bold underline'>This is HomePage</div>
     </>
   );
