c17ee45198d1939eb79640bf6627ed0316cd15f9
mazzziid <mazied2001@gmail.com>
2023-09-18 13:57:19 +0700

    add(sapiterdata): dropdown

--- src/pages/SapiTerdata/index.jsx
+++ src/pages/SapiTerdata/index.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../../components/Sidebar";
 import Button from "../../components/Button";
+import DropdownComponent from "../../components/Dropdown";
 
 export default function SapiTerdataRiwayat() {
     return (
@@ -8,16 +9,21 @@ export default function SapiTerdataRiwayat() {
         <div>
           <Sidebar />
         </div>
-          <div className=" mx-40 my-40 min-h-max">
+        
+          <div className=" mx-40 my-20 min-h-max">
+            <div className="mb-10 mr-40">
+            <DropdownComponent></DropdownComponent>
+            </div>
             <div className="flex justify-between">
             <Button className="bg-[#0D0D0D] w-[162px] h-[50px]">Analisa</Button>
             <Button className="bg-[#0D0D0D] w-[162px] h-[50px]">Tabel</Button>
             <Button className="text-black bg-[#AFC97E] w-[162px] h-[50px]">Riwayat</Button>
             </div>
-            <div>
-
+            <div className="flex mt-20">
+              <div className="text-black mr-[220px] ml-[140px]">Tanggal</div>
+              <div className="text-black">Kondisi</div>
             </div>
-            <div className="mt-20 flex">
+            <div className="mt-5 flex">
               <div className="text-black opacity-60 w-[332px] h-[53px] bg-[#b0b0b0] flex justify-center items-center rounded-md text-[20px]" >9/4/2022</div>
               <div className="w-[186px] h-[53px] bg-[#AFC97E] flex justify-center items-center rounded-md text-[20px] ml-4" >sehat</div>
             </div>
