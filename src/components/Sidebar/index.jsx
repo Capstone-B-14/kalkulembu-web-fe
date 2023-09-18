7de249b5b29169bb9f5ddb90435070fec12ae023
Vallentina Wahyu Febrihartanti <vallentina.wahyu15@gmail.com>
2023-09-18 23:51:42 +0700

    fix component: sidebar's style

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -9,10 +9,10 @@ export default function Sidebar() {
     return (
       <Link
         to={linkto}
-        className={`text-2xl flex items-center h-12 rounded-full justify-center transition-colors duration-300 ${
+        className={`text-2xl text-[#FBFBFB] font-medium flex items-center h-[53px] justify-center transition-colors duration-300 ${
           location.pathname === linkto
-            ? "font-semibold bg-gray-600 rounded-full text-white"
-            : "hover:bg-gray-600 hover:text-white font-normal"
+            ? "font-semibold bg-[#0D0D0D] rounded-r-xl text-[#75A3BF] hover:text-white"
+            : "hover:text-[#0D0D0D] font-semibold"
         }`}
         onClick={() => {
           setKey((prevKey) => prevKey + 1); // Update the key to trigger re-render
@@ -24,11 +24,11 @@ export default function Sidebar() {
   };
 
   return (
-    <div className='h-screen bg-blue-400 p-4 rounded justify-center' key={key}>
+    <div className='h-screen bg-[#75A3BF] p-4 rounded-r-2xl justify-center' key={key}>
       <div className='mx-auto w-32 h-32 mt-6 bg-gray-400 rounded-full flex items-center justify-center'>
         Logo
       </div>
-      <div className='w-64 my-12 space-y-2'>
+      <div className='w-64 my-12 space-y-4'>
         <SidebarLink text='Dashboard' linkto='/' />
         <SidebarLink text='Peternakan' linkto='/peternakan/analisis' />
         <SidebarLink text='Sapi Terdata' linkto='/sapi-terdata/tabel' />
@@ -41,7 +41,7 @@ export default function Sidebar() {
             console.log("Logout");
             setKey((prevKey) => prevKey + 1); // Update the key to trigger re-render
           }}
-          className='w-full h-16 bg-blue-500 text-white justify-center rounded-full font-bold text-2xl flex items-center'
+          className='w-full bg-[#75A3BF] text-white justify-center font-semibold text-2xl flex items-center hover:text-[#0D0D0D] hover:border-none'
         >
           Logout
         </button>
