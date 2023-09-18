9f4c924030c0c78008d0ec49e4c5a62028f08da6
Vallentina Wahyu Febrihartanti <vallentina.wahyu15@gmail.com>
2023-09-18 23:17:31 +0700

    add Pages: sapi-terdata-tabel

--- src/pages/SapiTerdata/riwayat.jsx
+++ src/pages/SapiTerdata/riwayat.jsx
@@ -14,13 +14,16 @@ export default function SapiTerdataRiwayat() {
             <div className="mb-10 mr-40">
             <DropdownComponent></DropdownComponent>
             </div>
-            <div className="flex justify-between">
+            {/* Navigation Button */}
+            <div>
               <NavButtonSapi></NavButtonSapi>
             </div>
+            {/* Table Header */}
             <div className="flex mt-20">
               <div className="text-black mr-[220px] ml-[140px]">Tanggal</div>
               <div className="text-black">Kondisi</div>
             </div>
+            {/* Table Row */}
             <div className="mt-5 flex">
               <div className="text-black opacity-60 w-[332px] h-[53px] bg-[#b0b0b0] flex justify-center items-center rounded-md text-[20px]" >9/4/2022</div>
               <div className="w-[186px] h-[53px] bg-[#AFC97E] flex justify-center items-center rounded-md text-[20px] ml-4" >sehat</div>
--- src/pages/SapiTerdata/tabel.jsx
+++ src/pages/SapiTerdata/tabel.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import Sidebar from "../../components/Sidebar";
-import NavButtonSapi from "../../components/NavButton/sapi"
+import DropdownComponent from "../../components/Dropdown";
+import NavButtonSapi from "../../components/NavButton/sapi";
+import Button from "../../components/Button";
 
 
 export default function SapiTabel() {
@@ -11,13 +13,27 @@ export default function SapiTabel() {
         <Sidebar></Sidebar>
       </div>
 
-      <div className="mt-20 mx-10">
-        <NavButtonSapi></NavButtonSapi>
-        {/* <div className="flex mt">
-          <div className="text-[#0D0D0D] font-bold mx-8">Tanggal</div>
-          <div className="text-[#0D0D0D] font-bold mx-8">Umur (Bulan)</div>
-          <div className="text-[#0D0D0D] font-bold mx-8">Bobot (kg)</div>
-        </div> */}
+      <div className="min-w-[370px] mx-16 my-20 flex flex-col">
+        <DropdownComponent></DropdownComponent>
+        <div className="mx-4 my-12">
+          <NavButtonSapi></NavButtonSapi>
+        </div>
+        {/* Table Header */}
+        <div className="flex flex-row min-w-max mx-4 mt-6 gap-x-2 pb-2">
+          <div className="flex basis-1/4 justify-center text-[#0D0D0D] font-bold text-lg mx-4 pl-1">Tanggal</div>
+          <div className="flex basis-1/4 justify-center text-[#0D0D0D] font-bold text-lg mx-5 pl-2">Umur (Bulan)</div>
+          <div className="flex basis-1/4 justify-center text-[#0D0D0D] font-bold text-lg pl-3">Bobot (kg)</div>
+          <div className="flex basis-1/4 justify-center text-[#0D0D0D] font-bold text-lg"></div>
+        </div>
+        {/* Table Row */}
+        <div className="bg-[#DDDDDD] gap-x-2 mx-4 rounded-lg h-[66px] w-[1004px] text-[#0D0D0D] text-[20px] flex justify-center items-center" >
+          <p className="flex basis-1/4 justify-center">9/14/2023</p>
+          <p className="flex basis-1/4 justify-center">12</p>
+          <p className="flex basis-1/4 justify-center">200</p>
+          <Button className="flex basis-1/5 justify-center items-center 
+                            h-[43px] bg-[#2E78A6] text-md text-[#FBFBFB]
+                            hover:bg-[#15547C]" onClick>Detail</Button>
+        </div>          
       </div>
     </div>
   );
