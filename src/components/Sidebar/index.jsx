f75741036345452b911e91b8a026680e204fcb25
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-09-19 17:24:32 +0700

    feat(sidebar): add app logo and change sidebar profile link

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Sidebar() {
   const [key, setKey] = useState(0); // Initialize key
@@ -11,37 +12,40 @@ export default function Sidebar() {
         to={linkto}
         className={`text-2xl text-[#FBFBFB] font-medium flex items-center h-[53px] justify-center transition-colors duration-300 ${
           location.pathname === linkto
-            ? "font-semibold bg-[#0D0D0D] rounded-r-xl text-[#75A3BF] hover:text-white"
-            : "hover:text-[#0D0D0D] font-semibold"
+            ? 'font-semibold bg-[#0D0D0D] rounded-r-xl text-[#75A3BF] hover:text-white'
+            : 'hover:text-[#0D0D0D] font-semibold'
         }`}
         onClick={() => {
           setKey((prevKey) => prevKey + 1); // Update the key to trigger re-render
         }}
       >
-        <span className='inline truncate hover:whitespace-normal'>{text}</span>
+        <span className="inline truncate hover:whitespace-normal">{text}</span>
       </Link>
     );
   };
 
   return (
-    <div className='h-screen bg-[#75A3BF] p-4 rounded-r-2xl justify-center' key={key}>
-      <div className='mx-auto w-32 h-32 mt-6 bg-gray-400 rounded-full flex items-center justify-center'>
-        Logo
+    <div
+      className="h-screen bg-[#75A3BF] p-4 rounded-r-2xl justify-center"
+      key={key}
+    >
+      <div className="flex items-center justify-center w-32 h-32 mx-auto mt-6">
+        <img src="/src/assets/kalkulembu-logo.png" alt="Logo" />
       </div>
-      <div className='w-64 my-12 space-y-4'>
-        <SidebarLink text='Dashboard' linkto='/' />
-        <SidebarLink text='Peternakan' linkto='/peternakan/analisis' />
-        <SidebarLink text='Sapi Terdata' linkto='/sapi-terdata/tabel' />
-        <SidebarLink text='Profil' linkto='/profil-peternak' />
+      <div className="w-64 my-12 space-y-4">
+        <SidebarLink text="Dashboard" linkto="/" />
+        <SidebarLink text="Peternakan" linkto="/peternakan/analisis" />
+        <SidebarLink text="Sapi Terdata" linkto="/sapi-terdata/tabel" />
+        <SidebarLink text="Profil" linkto="/profile" />
       </div>
       {/* Logout Button (or Login Button) */}
-      <div className='w-full'>
+      <div className="w-full">
         <button
           onClick={() => {
-            console.log("Logout");
+            console.log('Logout');
             setKey((prevKey) => prevKey + 1); // Update the key to trigger re-render
           }}
-          className='w-full bg-[#75A3BF] text-white justify-center font-semibold text-2xl flex items-center hover:text-[#0D0D0D] hover:border-none'
+          className="w-full bg-[#75A3BF] text-white justify-center font-semibold text-2xl flex items-center hover:text-[#0D0D0D] hover:border-none"
         >
           Logout
         </button>
