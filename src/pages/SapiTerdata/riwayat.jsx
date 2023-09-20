68c44a88151416b400cb0975ae5c14286f6a543f
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-09-20 23:55:58 +0700

    style(sapi-terdata): fix inconsistent layouts on riwayat

--- src/pages/SapiTerdata/riwayat.jsx
+++ src/pages/SapiTerdata/riwayat.jsx
@@ -1,36 +1,39 @@
-import Sidebar from "../../components/Sidebar";
-import DropdownComponent from "../../components/Dropdown";
-import NavButtonSapi from "../../components/NavButton/sapi";
+import Sidebar from '../../components/Sidebar';
+import DropdownComponent from '../../components/Dropdown';
+import NavButtonSapi from '../../components/NavButton/sapi';
 
 export default function SapiTerdataRiwayat() {
-    return (
-      <>
-        <div className='flex bg-[#FBFBFB]'>
-          <div>
-            <Sidebar />
-          </div>
+  return (
+    <>
+      <div className="flex bg-[#FBFBFB]">
+        <div>
+          <Sidebar />
+        </div>
 
-          <div className=" mx-40 my-20 min-h-max">
-            <div className="mb-10 mr-40">
-            <DropdownComponent></DropdownComponent>
+        <div className="min-w-[370px] mx-16 my-20 flex flex-col">
+          <DropdownComponent />
+          {/* Navigation Button */}
+          <div className="mx-4 my-12">
+            <NavButtonSapi />
+          </div>
+          {/* Table Header */}
+          <div className="flex">
+            <div className="text-black font-bold text-lg mr-[260px] ml-[135px]">
+              Tanggal
             </div>
-            {/* Navigation Button */}
-            <div>
-              <NavButtonSapi></NavButtonSapi>
+            <div className="text-lg font-bold text-black">Kondisi</div>
+          </div>
+          {/* Table Row */}
+          <div className="flex justify-between mt-2">
+            <div className="text-black w-[332px] h-[53px] bg-[#DDDDDD] flex justify-center items-center rounded-md text-[20px]">
+              9/4/2022
             </div>
-            {/* Table Header */}
-            <div className="flex mt-20">
-              <div className="text-black mr-[220px] ml-[140px]">Tanggal</div>
-              <div className="text-black">Kondisi</div>
-            </div>
-            {/* Table Row */}
-            <div className="mt-5 flex">
-              <div className="text-black opacity-60 w-[332px] h-[53px] bg-[#b0b0b0] flex justify-center items-center rounded-md text-[20px]" >9/4/2022</div>
-              <div className="w-[186px] h-[53px] bg-[#AFC97E] flex justify-center items-center rounded-md text-[20px] ml-4" >sehat</div>
+            <div className="w-[186px] h-[53px] bg-[#AFC97E] flex justify-center items-center rounded-md text-[20px] ml-4 uppercase">
+              sehat
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-  
+      </div>
+    </>
+  );
+}
