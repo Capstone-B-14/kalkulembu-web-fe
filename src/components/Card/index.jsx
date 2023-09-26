868490979587d01c2a46f18e59d728b3173290f3
dindasabela <dindasabela02@mail.ugm.ac.id>
2023-09-26 19:10:32 +0700

    fix component : import card component and cardData to homepage

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -19,31 +19,49 @@ import React from "react";
 //   );
 // }
 
-const Card = (props) => {
-  return (
-    <div>
-      {props.details.map((value, index) => (
-        <div
-          key={index}
-          className='inline-block h-[300px] relative grid-cols-3 bg-[#fbfbfb] rounded-md mx-3 my-2 overflow-x-auto shadow-lg'
-        >
-          <img
-            src={value.img}
-            alt='img'
-            className='rounded-md rounded-b-none h-48 w-48 object-center'
-          />
-          <div>
-            <h1 className='font-semibold text-lg'>{value.title}</h1>
-            <h2 className='text-sm mt-1'>{value.address}</h2>
-            <button className='bg-lime-300 text-white px-2 py-1 rounded-full text-xs mt-2 '>
-              Lihat Selengkapnya
-            </button>
-          </div>
-        </div>
-      ))}
-      ;
-    </div>
-  );
-};
+//const Card = (props) => {
+  //return (
+    //<div>
+      //{props.details.map((value, index) => (
+        //<div
+          //key={index}
+          //className='inline-block h-[350px] w-[300px] relative bg-[#fbfbfb] rounded-md mx-3 my-2 shadow-lg p-3'
+        //>
+          //<img
+            //src={value.img}
+            //alt='img'
+            //className='rounded-md rounded-b-none object-center h-[200px] w-full'
+          // />
+          //<div>
+            //<h1 className='font-semibold text-lg break-words'>{value.title}</h1>
+            //<h2 className='text-sm mt-1'>{value.address}</h2>
+            //<button className='bg-lime-300 text-white px-2 py-1 rounded-full text-xs mt-2 '>
+              //Lihat Selengkapnya
+            //</button>
+          //</div>
+        //</div>
+      //))}
+      //;
+    //</div>
+  //);
+//};
 
+//export default Card;
+
+const Card = (cardData) => {
+  const {title, children, img, action} = cardData;
+  return (
+    //<A>
+    <div className="inline-block h-[350px] w-[300px] relative bg-[#fbfbfb] rounded-md mx-3 my-2 shadow-lg p-3">
+      <img
+      src={img}
+        className="rounded-md rounded-b-none object-center h-[200px] w-full"
+      ></img>
+      <h1 className="font-semibold text-lg break-words">{title}</h1>
+        <h2 className="text-sm mt-1">{children}</h2>
+      <button className="mt-3 bg-lime-300 border-lime-100 hover:bg-lime-200">{action}</button>
+    </div>
+    //</A>
+  );
+}
 export default Card;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,27 @@
 import Sidebar from "../../components/Sidebar";
 import Card from "../../components/Card";
-import cardData from "../../components/Card/cardData.jsx";
+import {cardData} from "../../components/Card/cardData.jsx";
+import React, {useState} from 'react';
 
 export default function HomePage() {
+  const [data] = useState(cardData);
   return (
-    <div className="flex bg-[#FBFBFB]">
-      <Sidebar />
-      <Card details={cardData} />
-    </div>
+    <>
+      <div className="flex bg-[#FBFBFB]">
+        <Sidebar />
+        {data.map((value, index) => {
+          return (
+            <div key={index}>
+              <Card
+                title={value.title}
+                children={value.address}
+                img={value.img}
+                action={"Lihat Selengkapnya"}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
