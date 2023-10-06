4aa25b1a4857e4acaf72b66fb1a9f92b40330506
mazzziid <mazied2001@gmail.com>
2023-10-06 14:56:39 +0700

    feat(components): create card table components

---
+++ src/components/CardTabel/cardTabel.jsx
@@ -0,0 +1,12 @@
+import sapi1 from '../../assets/sapi1.jpg';
+
+
+export const cardData = [
+  {
+    title: "Bejo si Sapi", 
+    age: "24 tahun", 
+    img: sapi1, 
+    bobot: "500 kg", 
+    condition: "sehat",
+  },
+];
