5e8f51398f87456cd4f74639d751ada9e06d5b38
dindasabela <dindasabela02@mail.ugm.ac.id>
2023-11-09 22:25:10 +0700

    feat : create Pagination component

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,28 @@
+import ReactPaginate from "react-paginate";
+import {BsChevronLeft, BsChevronRight} from "react-icons/bs";
+
+const Pagination = () => {
+    return (
+      <div className="text-lg font-semibold">
+        <ReactPaginate
+          breakLabel={<span className="mx-2">...</span>}
+          nextLabel={
+            <span>
+              <BsChevronRight className="text-xl" />
+            </span>
+          }
+          pageCount={10}
+          previousLabel={
+            <span>
+              <BsChevronLeft className="text-xl" />
+            </span>
+          }
+          renderOnZeroPageCount={null}
+          containerClassName="items-center flex mt-6 mb-5 ml-[10px]"
+          pageClassName="w-8 h-2 flex items-center rounded-md ml-3 hover:bg-lightGray"
+        />
+      </div>
+    );
+};
+
+export default Pagination; 
